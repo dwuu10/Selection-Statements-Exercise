6ed674ca5f058675d3e463353170ea6e0adf0c0c
dylan wuu <dylanwuu@oulook.com>
2023-06-23 15:31:35 -0700

    exercise complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("guess a number between 1 and 100");
+            var random = new Random();
+            var number = random.Next(1, 100);
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput > number)
+            {
+                Console.WriteLine("your guess is too high");
+            }
+            else if (userInput < number)
+            {
+                Console.WriteLine("your guess is too low");
+            }
+            else
+            {
+                Console.WriteLine("congratulations, you got the right number");
+            }
         }
     }
 }
